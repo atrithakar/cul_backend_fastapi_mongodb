d219776f23a387b74188b4e2ede3d713bd317d6f
Atri Thakar <atrithaker122@gmail.com>
2025-07-08 19:20:04 +0530

    Getting Ready To Deploy

--- app/main.py
+++ app/main.py
@@ -11,25 +11,14 @@ app = FastAPI()
 app.include_router(api_router)
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
 app.mount("/templates", StaticFiles(directory="app/templates"), name="templates")
-app.add_middleware(SessionMiddleware, secret_key="atri")
-
 app.add_middleware(
     SessionMiddleware,
-    secret_key = "atri",
-    max_age = 60 * 60 * 24 * 15
+    secret_key=os.getenv("SESSION_SECRET_KEY", "default_dev_key"),
+    max_age=60 * 60 * 24 * 15
 )
 
-def get_host_ip():
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    try:
-        # doesn't even have to be reachable
-        s.connect(('10.254.254.254', 1))
-        ip = s.getsockname()[0]
-    except Exception:
-        ip = '127.0.0.1'
-    finally:
-        s.close()
-    return ip
 
 if __name__ == "__main__":
-    uvicorn.run(app, host=get_host_ip(), port=8000)
+    import os
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
