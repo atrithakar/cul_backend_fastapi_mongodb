adac333cbe7e0bc25aa3212a94fe5850bf7e5768
Atri Thakar <atrithaker122@gmail.com>
2025-07-08 19:07:48 +0530

    Getting Ready To Deploy

--- app/database.py
+++ app/database.py
@@ -1,17 +1,25 @@
-'''
-This file contains the code to connect to the MongoDB database and get the next sequence value for the _id field of the documents.
-'''
 from motor.motor_asyncio import AsyncIOMotorClient
 from pydantic_settings import BaseSettings
+from dotenv import load_dotenv
+import os
 
 class Settings(BaseSettings):
-    MONGODB_URL: str = "mongodb://localhost:27017"
-    DATABASE_NAME: str = "cul_db"
+
+    load_dotenv()
+
+    DB_USER: str = os.getenv("DB_USER")
+    DB_PASSWORD: str = os.getenv("DB_PASSWORD")
+    DB_CLUSTER: str = os.getenv("DB_CLUSTER")
+    DB_NAME: str = os.getenv("DB_NAME")
+
+    @property
+    def MONGODB_URL(self):
+        return f"mongodb+srv://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_CLUSTER}.mongodb.net/?retryWrites=true&w=majority&appName=cul-cluster"
 
 settings = Settings()
 
 client = AsyncIOMotorClient(settings.MONGODB_URL)
-database = client[settings.DATABASE_NAME]
+database = client[settings.DB_NAME]
 
 def get_database():
     return database
@@ -23,4 +31,4 @@ async def get_next_sequence_value(sequence_name: str):
         upsert=True,
         return_document=True
     )
-    return sequence_document["sequence_value"]
+    return sequence_document.get("sequence_value", 1)
