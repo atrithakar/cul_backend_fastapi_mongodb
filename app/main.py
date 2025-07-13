fdc98e644780c3e878757bf0f1abdb5c7806eb89
Atri Thakar <atrithaker122@gmail.com>
2025-07-13 20:53:12 +0530

    Minor changes for ease of debugging

--- app/main.py
+++ app/main.py
@@ -29,3 +29,4 @@ app.add_middleware(
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
     uvicorn.run("main:app", host="0.0.0.0", port=port)
+    # uvicorn.run("main:app", host="127.0.0.1", port=port)
